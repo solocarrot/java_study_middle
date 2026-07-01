250ea05e1874fecbc56172a331e399544ef2d2bd
solocarrot <whddbs3595@naver.com>
2026-07-01 15:51:45 +0900

    중첩클래스1 완성했음

--- src/nested/test/OuterClass1.java
+++ src/nested/test/OuterClass1.java
@@ -1,4 +1,9 @@
 package nested.test;
 
 public class OuterClass1 {
+    public static class NestedClass {
+        public void hello() {
+            System.out.println("hello");
+        }
+    }
 }
