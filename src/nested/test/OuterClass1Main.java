626efaf2bb7498346fec4fbeb169d32e5573f718
solocarrot <whddbs3595@naver.com>
2026-07-01 15:55:00 +0900

    중첩클래스1메인 완성했음

--- src/nested/test/OuterClass1Main.java
+++ src/nested/test/OuterClass1Main.java
@@ -2,6 +2,7 @@ package nested.test;
 
 public class OuterClass1Main {
     public static void main(String[] args) {
-
+        OuterClass1.NestedClass outerClass1 = new OuterClass1.NestedClass();
+        outerClass1.hello();
     }
 }
