759e55c36e2993c1d92d15e0bff628749a418c67
종윤 <whddbs3595@naver.com>
2026-07-07 22:39:39 +0900

    class3 1차 완성해씅ㅁ

--- src/nested/test/OuterClass3.java
+++ src/nested/test/OuterClass3.java
@@ -2,10 +2,14 @@ package nested.test;
 
 public class OuterClass3 {
     public void myMethod () {
-        public class LocalClass {
+        class LocalClass {
             public void hello() {
-
+//                outerclass.mymethod만해도 hello가 출력이 되게끔해야되니까. 메서드안에 클래스가있는거고
+//                그클래스안에 또 HELLO라는 메서드가있는데 이걸 호출하지않아도 안에 내용이 출력이될수있는건가? 엥
+                System.out.println("localclass.hello");
             }
         }
+        LocalClass localClass = new LocalClass();
+        localClass.hello();
     }
 }
