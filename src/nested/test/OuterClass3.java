cb24e52bb459124cd8ba33ea1aa00104da973e4c
종윤 <whddbs3595@naver.com>
2026-07-07 22:29:53 +0900

    class3만들었음

---
+++ src/nested/test/OuterClass3.java
@@ -0,0 +1,11 @@
+package nested.test;
+
+public class OuterClass3 {
+    public void myMethod () {
+        public class LocalClass {
+            public void hello() {
+
+            }
+        }
+    }
+}
