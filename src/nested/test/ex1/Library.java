ce121fcdf5d83dbae41ece7312fc2b0fe5210298
종윤 <whddbs3595@naver.com>
2026-07-14 17:15:34 +0900

    2번과제 혼자서 시도했지만 개같이 실패했음

--- src/nested/test/ex1/Library.java
+++ src/nested/test/ex1/Library.java
@@ -1,10 +1,17 @@
 package nested.test.ex1;
 
 public class Library {
-    String title;
-    String author;
-    Library(String title, String author) {
-        this.title = title;
-        this.author = author;
+    //addbook으로해서 메서드에있어야되는거고 건설자에는 숫자입력하면 그만큼의 북을 가지는 집합을 만들어야되는거잖아 그러면 라이브러리는 집합
+    static class Book{
+        String title;
+        String author;
+        void addBook(String title, String author) {
+            this.title = title;
+            this.author = author;
+        }
     }
+    Library(int N) {
+
+    }
+
 }
--- src/nested/test/ex1/LibraryMain.java
+++ src/nested/test/ex1/LibraryMain.java
@@ -2,6 +2,7 @@ package nested.test.ex1;
 
 public class LibraryMain {
     public static void main(String[] args) {
+        Library library = new Library(4)
 
     }
 }
