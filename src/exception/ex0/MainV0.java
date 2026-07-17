f9a41f7bde26af23821d970529debf7d67b92dc9
종윤 <whddbs3595@naver.com>
2026-07-17 12:57:13 +0900

    pdf보고 수정헀음

--- src/exception/ex0/MainV0.java
+++ src/exception/ex0/MainV0.java
@@ -3,14 +3,20 @@ package exception.ex0;
 import java.util.Scanner;
 
 public class MainV0 {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         NetworkServiceV0 networkServiceV0 = new NetworkServiceV0();
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            networkServiceV0.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램종료");
 
-        networkServiceV0.sendMessage(input);
     }
 }
