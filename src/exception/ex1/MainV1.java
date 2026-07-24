7ca4cfaf71c0e402271bc34944ad342f76775f8a
solocarrot <rlawhddbs3339@gmail.com>
2026-07-25 00:28:18 +0900

    메인문 수정했음 먼저 입력안받는걸 해결하고 무한히 반복되는 문제를 nextLine을 while문안에넣어서해결했음

--- src/exception/ex1/MainV1.java
+++ src/exception/ex1/MainV1.java
@@ -6,12 +6,14 @@ public class MainV1 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        NetworkSeverV1_1 severV1_1 = new NetworkSeverV1_1();
-        severV1_1.sendMessage("hello");
-
-        NetworkSeverV1_1 severV1_2 = new NetworkSeverV1_1();
-        severV1_2.sendMessage("error1");
-
-        severV1_2.sendMessage("error2");
+        NetworkSeverV1_1 severV1_1 = new NetworkSeverV1_1();h
+        while(true) {
+            String data = scanner.nextLine();
+            if (data.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+            severV1_1.sendMessage(data);
+        }
     }
 }
