bff6a0ba5162cae4c3208530cd436ece07aee03e
solocarrot <rlawhddbs3339@gmail.com>
2026-07-26 03:02:55 +0900

    버전1-3의 계획표만들었음

--- src/exception/ex1/MainV1.java
+++ src/exception/ex1/MainV1.java
@@ -6,7 +6,7 @@ public class MainV1 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        NetworkSeverV1_2 severV1_1 = new NetworkSeverV1_2();
+        NetworkSeverV1_2 severV1_2 = new NetworkSeverV1_2();
         while(true) {
             System.out.print("전송할 문자 : ");
             String data = scanner.nextLine();
@@ -14,7 +14,7 @@ public class MainV1 {
                 System.out.println("프로그램을 종료합니다.");
                 break;
             }
-            severV1_1.sendMessage(data);
+            severV1_2.sendMessage(data);
         }
     }
 }
