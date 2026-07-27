1e63f2618e2448138458ca572800df14f8efb4ff
solocarrot <rlawhddbs3339@gmail.com>
2026-07-28 03:57:51 +0900

    V2서버 만들었음

---
+++ src/exception/ex2/NetworkSeverV2_1.java
@@ -0,0 +1,16 @@
+package exception.ex2;
+
+public class NetworkSeverV2_1 {
+
+    public void sendMessage(String data) throws NetworkClientExceptionV2{
+        String address = "http://example.com";
+
+        NetworkClientV2 networkClientV2 = new NetworkClientV2(address);
+        networkClientV2.initError(data);
+
+        networkClientV2.connect();
+        networkClientV2.send(data);
+        networkClientV2.disconnect();
+    }
+
+}
