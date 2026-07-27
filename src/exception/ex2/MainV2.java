60295a3e73ce00d819e1f2e859e83976c62c8f23
solocarrot <rlawhddbs3339@gmail.com>
2026-07-28 04:05:25 +0900

    while true문으로 입력받는 부분까지 완성했음

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -7,8 +7,16 @@ public class MainV2 {
         Scanner scanner = new Scanner(System.in);
 
         NetworkSeverV2_1 sever = new NetworkSeverV2_1();
-        sever.sendMessage("hello");
-
+        while(true) {
+            System.out.println("전송할 문자");
+            String data = scanner.nextLine();
+            if(data.equals("exit")) {
+                break;
+            }
+            sever.sendMessage(data);
+            System.out.println();
+        }
+        System.out.println("프로그램을 정상 종료합니다");
     }
 
 }
