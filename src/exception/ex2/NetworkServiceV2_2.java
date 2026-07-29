9769a698782fe5bf1a240121779b0a7403ae673f
solocarrot <rlawhddbs3339@gmail.com>
2026-07-29 12:22:13 +0900

    V2_2 보고 따라적었음 try catch용법 정도배움

--- src/exception/ex2/NetworkServiceV2_2.java
+++ src/exception/ex2/NetworkServiceV2_2.java
@@ -2,14 +2,24 @@ package exception.ex2;
 
 public class NetworkServiceV2_2 {
 
-    public void sendMessage(String data) throws NetworkClientExceptionV2{
+    public void sendMessage(String data){
         String address = "http://example.com";
 
         NetworkClientV2 networkClientV2 = new NetworkClientV2(address);
         networkClientV2.initError(data);
 
-        networkClientV2.connect();
-        networkClientV2.send(data);
+        try{
+            networkClientV2.connect();
+        } catch (NetworkClientExceptionV2 e) {
+            System.out.println("오류코드: " + e.getErrorCode() + "오류메시지 : " +e.getMessage());
+            return;
+        }
+        try {
+            networkClientV2.send(data);
+        } catch (NetworkClientExceptionV2 e) {
+            System.out.println("오류코드: " + e.getErrorCode() + "오류메시지 : " +e.getMessage());
+            return;
+        }
         networkClientV2.disconnect();
     }
 
