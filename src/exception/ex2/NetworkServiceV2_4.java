3df91956e7676d3b7e9f84b749cfdb670f52b696
solocarrot <rlawhddbs3339@gmail.com>
2026-07-29 12:44:13 +0900

    2_4 따라침 예상되지않은 오류만나면 catch로 못잡으면 메서드가 강제종료된다는걸 알았음

---
+++ src/exception/ex2/NetworkServiceV2_4.java
@@ -0,0 +1,21 @@
+package exception.ex2;
+
+public class NetworkServiceV2_4 {
+
+    public void sendMessage(String data){
+        String address = "http://example.com";
+
+        NetworkClientV2 networkClientV2 = new NetworkClientV2(address);
+        networkClientV2.initError(data);
+
+        try{
+            networkClientV2.connect();
+            networkClientV2.send(data);
+        } catch (NetworkClientExceptionV2 e) {
+            System.out.println("오류코드: " + e.getErrorCode() + "오류메시지 : " +e.getMessage());
+        }
+        networkClientV2.disconnect();
+
+    }
+
+}
