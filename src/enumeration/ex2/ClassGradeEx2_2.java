46c99fa2f8f5600e88f92d390d0b4fbb093c8dc4
solocarrot <rlawhddbs3339@gmail.com>
2026-08-01 00:13:51 +0900

    ClassGrade에 private없으니까 다른클래스에서 new ClassGrade가 가능한 문제발생

---
+++ src/enumeration/ex2/ClassGradeEx2_2.java
@@ -0,0 +1,19 @@
+package enumeration.ex2;
+
+public class ClassGradeEx2_2 {
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        ClassGrade resultClass = new ClassGrade();
+        int result = discountService.discount(resultClass, price);
+        int basic = discountService.discount(ClassGrade.BASIC, price);
+        int gold = discountService.discount(ClassGrade.GOLD, price);
+        int diamond = discountService.discount(ClassGrade.DIAMOND, price);
+
+        System.out.println("BASIC등급의할인금액" + basic);
+        System.out.println("RESULT등급의할인금액" + result);
+        System.out.println("GOLD등급의할인금액" + gold);
+        System.out.println("DIAMOND등급의할인금액" + diamond);
+    }
+}
---
+++ src/enumeration/ex2/DiscountService.java
@@ -0,0 +1,19 @@
+package enumeration.ex2;
+
+public class DiscountService {
+
+    public int discount(ClassGrade grade, int price) {
+        int discountPercent = 0;
+        if(grade == ClassGrade.BASIC) {
+            discountPercent = 10;
+        } else if(grade == ClassGrade.GOLD) {
+            discountPercent = 20;
+        } else if(grade == ClassGrade.DIAMOND) {
+            discountPercent = 30;
+        } else {
+            System.out.println(grade + "할인x");
+        }
+
+        return price * discountPercent / 100;
+    }
+}
