9d9391b2289a95219a80b772f5c8315f9f21ecf2
solocarrot <rlawhddbs3339@gmail.com>
2026-08-05 01:37:17 +0900

    제네릭 문제와풀이1까지 따라적음

---
+++ src/generic/test/ex1/Container.java
@@ -0,0 +1,21 @@
+package generic.test.ex1;
+
+public class Container<T> {
+
+    private T item;
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public boolean isEmpty() {
+        if(item == null) {
+            return true;
+        }
+        return false;
+    }
+}
