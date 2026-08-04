6581c1ae64e20465c518885e024a9dbc8c2153fe
solocarrot <rlawhddbs3339@gmail.com>
2026-08-05 01:57:59 +0900

    문제와풀이2 따라침

---
+++ src/generic/test/ex1/Pair.java
@@ -0,0 +1,31 @@
+package generic.test.ex1;
+
+public class Pair<T1, T2> {
+
+    private T1 first;
+    private T2 second;
+
+    public void setFirst(T1 value) {
+        this.first = value;
+    }
+
+    public void setSecond(T2 value) {
+        this.second = value;
+    }
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
