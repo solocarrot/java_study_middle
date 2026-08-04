fdc03445de8e0d4cca432823f93e95b29ca55a91
solocarrot <rlawhddbs3339@gmail.com>
2026-08-04 13:36:40 +0900

    <T>사용하는 법 배움 T가 String int Double로 바뀐다고 생가각하면될듯, 타입추론으로 new 뒤에 <>에 내용안없어도됨

---
+++ src/generic/ex1/BoxMain3.java
@@ -0,0 +1,25 @@
+package generic.ex1;
+
+public class BoxMain3 {
+
+    public static void main(String[] args) {
+        GenericBox<Integer> integerBox = new GenericBox<Integer>();
+        integerBox.set(10);
+        Integer integer = integerBox.get();
+        System.out.println("integer = " + integer);
+
+        GenericBox<String> stringBox = new GenericBox<String>();
+        stringBox.set("hello");
+        String str = stringBox.get();
+        System.out.println("str = " + str);
+
+        GenericBox<Double> doubleBox = new GenericBox<Double>();
+        doubleBox.set(1.1);
+        Double doubleValue = doubleBox.get();
+        System.out.println("double = " + doubleValue);
+
+        GenericBox<Integer> integerGenericBox = new GenericBox<>();
+
+
+    }
+}
