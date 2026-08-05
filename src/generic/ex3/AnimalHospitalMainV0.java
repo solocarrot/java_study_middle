620880a534130db17ba28ede29ee13f250f34d7e
solocarrot <rlawhddbs3339@gmail.com>
2026-08-05 09:47:48 +0900

    catHostipal로만 사용해보고 Animalhospital로도 사용해봤는데 안정성문제가 발생해서 T로 AnimalHostipalV2를 하니까 animal에있는 메서드를 T로 선언한 시점에서는 사용할수가없고 Cat 이나 Dog 뿐 아니라 integer String등도 들어갈수있는 문제가 있다

---
+++ src/generic/ex3/AnimalHospitalMainV0.java
@@ -0,0 +1,24 @@
+package generic.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV0 {
+
+    public static void main(String[] args) {
+        CatHospital catHospital = new CatHospital();
+        DogHospital dogHospital = new DogHospital();
+
+        Dog dog = new Dog("멍멍이1", 100);
+        Cat cat = new Cat("냐옹이1", 300);
+
+        dogHospital.set(dog);
+        dogHospital.checkup();
+
+        catHospital.set(cat);
+        catHospital.checkup();
+
+        Dog biggerDog = dogHospital.bigger(new Dog("멍멍이2", 200));
+        System.out.println("biggerDog : " + biggerDog);
+    }
+}
