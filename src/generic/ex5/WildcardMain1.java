ef4fd52bb829312e7161d6908412f4285bfb111f
solocarrot <rlawhddbs3339@gmail.com>
2026-08-06 12:18:45 +0900

    제네릭메서드와 와일드카드메서드 차이점은 제네릭메서드는 인자확인해서 인자의 타입으로 T를 모두 바꾼다음에 실행되지만 와일드카드는 그냥 메서드처럼 작동함

---
+++ src/generic/ex5/WildcardMain1.java
@@ -0,0 +1,25 @@
+package generic.ex5;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildcardMain1 {
+
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Dog> dogBox = new Box<Dog>();
+        Box<Cat> catBox = new Box<Cat>();
+        dogBox.set(new Dog("멍멍이1", 100));
+
+        WildcardEx.<Dog>printGenericV1(dogBox);
+        WildcardEx.printWildcardV1(dogBox);
+        WildcardEx.printGenericV2(dogBox);
+        WildcardEx.printWildcardV2(dogBox);
+
+        Dog dog1 = WildcardEx.printAndReturnGeneric(dogBox);
+        Animal dog2 = WildcardEx.printAndReturnWildcard(dogBox);
+
+
+    }
+}
