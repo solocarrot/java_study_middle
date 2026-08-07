a89aaa767e377fe7cb72955f16b95c6c7563a870
solocarrot <rlawhddbs3339@gmail.com>
2026-08-08 00:33:04 +0900

    두정수사이의 합 만듦

---
+++ src/progammers/Solution.java
@@ -0,0 +1,17 @@
+package progammers;
+
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        //a와 b에 속한 모두 정수의 합을 리턴하기.
+        //조건1. 같은경우 아무거나하기, 조건2 대소관계정해져있지않음.
+        long bigNum = Math.max(a,b);
+        long smallNum = Math.min(a,b);
+        for(long i = smallNum; i <= bigNum; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+
+
+}
