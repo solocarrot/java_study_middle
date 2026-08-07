0acffaa19094227b2ea8222714d0731a8a7b0d7d
solocarrot <rlawhddbs3339@gmail.com>
2026-08-08 00:51:30 +0900

    문자를 정수로 만들기 문제 푸는중 아스키코드써야된다는 힌트받음, isSing으로 부호확인하고 isPlus로 *1 이나 *-1할생각임

---
+++ src/progammers/Solution12925.java
@@ -0,0 +1,19 @@
+package progammers;
+
+public class Solution12925 {
+    public int solution(String s) {
+        //문자열 s를 숫자로 변환시키기
+        int answer = 0;
+        int arrayLength = s.length();
+        char[] chars = new char[arrayLength];
+
+        return answer;
+    }
+
+    public boolean isSign(char c) {
+        if(c == '+' || c == '-') {
+            return true;
+        }
+        return false;
+    }
+}
