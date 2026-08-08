9dc0d6d80890e73c0d1e9f5a05aab9b6eb5b102e
solocarrot <rlawhddbs3339@gmail.com>
2026-08-08 22:15:20 +0900

    배열늘리는 코드랑 add에서 size가 길이랑 같으면 늘려주기

---
+++ src/collection/array/MyArrayListV2.java
@@ -0,0 +1,69 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class MyArrayListV2 {
+
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayListV2() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayListV2(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(Object e) {
+        //무슨일이 벌어지는지보자 2칸짜리 배열이있어 사이즈는0인데 1추가하면 1 null 사이즈는 1 / 1 2 2/
+        //여기서한번더 add를하면 이제 메서드에서확인을하는거지 사이즈가 지금2에요 하면늘리기
+        if(size == elementData.length) {
+            grow();
+        }
+        elementData[size] = e;
+        size++;
+    }
+
+    public void grow() {
+        Object[] newElementData = new Object[elementData.length * 2];
+        for(int i = 0; i <size; i++) {
+            newElementData[i] = elementData[i];
+        }
+        elementData = newElementData;
+    }
+
+    public Object get(int index) {
+        return elementData[index];
+    }
+
+    public Object set(int index, Object element) {
+        Object oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    public int indexOf(Object o) {
+        for(int i = 0; i < elementData.length; i++) {
+            if(elementData[i] == o) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return "MyArrayListV1{" +
+                "elementData=" + Arrays.toString(elementData) +
+                ", size=" + size +
+                ", capacity = " + elementData.length +
+                '}';
+    }
+}
