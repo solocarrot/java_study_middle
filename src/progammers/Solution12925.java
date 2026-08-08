3bb560c78450e46a82d394ee5860d29a5c90338a
solocarrot <rlawhddbs3339@gmail.com>
2026-08-09 02:19:27 +0900

    부호메서드를 만드는 게 너무어려움

--- src/progammers/Solution12925.java
+++ src/progammers/Solution12925.java
@@ -3,17 +3,45 @@ package progammers;
 public class Solution12925 {
     public int solution(String s) {
         //문자열 s를 숫자로 변환시키기
+        //ex1) 12345가입력됨. 문자열을 char의 배열으로 집어넣고싶다.
         int answer = 0;
         int arrayLength = s.length();
-        char[] chars = new char[arrayLength];
+
+
+        char[] charArray = new char[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            charArray[i] = s.charAt(i);
+        }
+
+        int superScript = 5;
+        for(int i = 0; i < arrayLength; i++) {
+            if(isSign(charArray[i])) {
+                superScript--;
+
+            }
+            int num1 = charArray[i] - 48;
+            int num2 = superScript(superScript);
+            superScript--;
+            answer += num1 * num2;
+        }
 
         return answer;
     }
 
-    public boolean isSign(char c) {
-        if(c == '+' || c == '-') {
-            return true;
+    private char isSign(char c) {
+        if(c == '+' ) {
+            return ' ';
+        } else if (c == '-') {
+            return '-';
         }
-        return false;
+    }
+
+    private int superScript(int n) {
+        int result = 1;
+        while(n == 1) {
+            result = result * 10;
+            n--;
+        }
+        return result;
     }
 }
