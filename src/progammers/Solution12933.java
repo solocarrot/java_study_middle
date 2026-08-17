2a9a3f4c685286d3524043f6309b2bfd696d0214
solocarrot <rlawhddbs3339@gmail.com>
2026-08-17 14:50:38 +0900

    정수 내림차순에서 벽느낌

---
+++ src/progammers/Solution12933.java
@@ -0,0 +1,20 @@
+package progammers;
+
+public class Solution12933 {
+    public long solution(long n) {
+        long temp = n;
+        int numberLength = 0;
+        while(temp > 0) {
+            temp = temp / 10;
+            numberLength++;
+        }
+        int[] nArray = new int[numberLength];
+        int temp2 = numberLength - 1;
+        for(int i = 0; i < numberLength; i++) {
+
+
+        }
+        long answer = 0;
+        return answer;
+    }
+}
